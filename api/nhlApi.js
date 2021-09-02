489f5c651aeaeeee19481c15383cad70bb681842
Jeremy Cook <jcook61592@gmail.com>
2021-09-02 15:48:41 -0400

    added more api calls for nhl players and teams

--- api/nhlApi.js
+++ api/nhlApi.js
@@ -15,3 +15,50 @@ export const nhlPlayerRetrieval = () => {
     });
   console.log(playerArr);
 };
+
+export const nhlTeamRetrieval = () => {
+  let teamArr = [];
+  axios.get('https://statsapi.web.nhl.com/api/v1/teams').then((res) => {
+    const teams = res.data.teams;
+    for (const team of teams) {
+      let singleTeam = team.name;
+      teamArr.push(singleTeam);
+    }
+  });
+  console.log(teamArr);
+};
+
+export const singleNhlTeamRetrieval = (id) => {
+  axios
+    .get(`https://statsapi.web.nhl.com/api/v1/teams/${id}?=team.stats`)
+    .then((res) => {
+      console.log(res);
+    });
+};
+// name, venue[name], venue[city], division[name], first year of play, conference[name], officialSiteUrl
+
+export const singleNhlPlayerRetrieval = (id) => {
+  let rosterArr = [];
+  axios
+    .get(`https://statsapi.web.nhl.com/api/v1/teams/${id}?expand=team.roster`)
+    .then((res) => {
+      const teamRoster = res.data.teams.roster;
+      for (const players of teamRoster) {
+        let names = players.roster.person.fullName;
+        rosterArr.push(names);
+      }
+    });
+  console.log(rosterArr);
+};
+// name, jersey number, position,
+
+export const singlePlayerStatRetrieval = (id, season = 20202021) => {
+  axios
+    .get(
+      `https://statsapi.web.nhl.com/api/v1/people/${id}/stats?stats=statsSingleSeason&season=${season}`
+    )
+    .then((res) => {
+      console.log(res);
+    });
+};
+// pretty much every stat that is listed. If they made the postseason, show their postseason stats as well

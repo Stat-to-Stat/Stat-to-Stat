d313af73dc193b489408d997721ba4ce82efb66a
Jeremy Cook <jcook61592@gmail.com>
2021-09-07 14:23:22 -0400

    Stats populating for both players and goalies by position

--- src/Pages/PlayerToPlayer/NHL/index.js
+++ src/Pages/PlayerToPlayer/NHL/index.js
@@ -1,43 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import Search from "./components/Search"
-import Filter from "./components/Filter"
+import Search from './components/Search';
+import Filter from './components/Filter';
 import {
-    nhlPlayerRetrieval,
-    singlePlayerStatRetrieval,
-    nhlTeamRetrieval,
-  } from '../../../api/nhlApi';
+  nhlPlayerRetrieval,
+  singlePlayerStatRetrieval,
+  nhlTeamRetrieval,
+} from '../../../api/nhlApi';
 
 function NHLPlayerToPlayer() {
   const [currentPlayerOne, setCurrentPlayerOne] = useState('');
   const [currentPlayerTwo, setCurrentPlayerTwo] = useState('');
-  
+
   const [playerList, setPlayerList] = useState([]);
   const [teamList, setTeamList] = useState([]);
-  
-  useEffect(() => {
-    const setArrays = async() => {
-        setPlayerList(await nhlPlayerRetrieval());
-        setTeamList(await nhlTeamRetrieval())
-    }
-    setArrays()
-}, [])
 
+  useEffect(() => {
+    const setArrays = async () => {
+      setPlayerList(await nhlPlayerRetrieval());
+      setTeamList(await nhlTeamRetrieval());
+    };
+    setArrays();
+  }, []);
 
   return (
     <div>
-        <Filter teamList={teamList} />
-        <Search  
-        setCurrentPlayer={setCurrentPlayerOne} 
+      <Filter teamList={teamList} />
+      <Search
+        setCurrentPlayer={setCurrentPlayerOne}
         playerList={playerList}
         teamList={teamList}
-        />
-        <Search  
-        setCurrentPlayer={setCurrentPlayerTwo} 
+      />
+      <Search
+        setCurrentPlayer={setCurrentPlayerTwo}
         playerList={playerList}
         teamList={teamList}
-        />
+      />
     </div>
-      )
+  );
 }
 
 export default NHLPlayerToPlayer;

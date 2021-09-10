c61547f040a61133c21d27b84e49135777b7b9b8
Jeremy Cook <jcook61592@gmail.com>
2021-09-10 19:26:04 -0400

    optimized player and goalie components to match team component

--- src/Pages/PlayerToPlayer/NHL/components/Goalies.js
+++ src/Pages/PlayerToPlayer/NHL/components/Goalies.js
@@ -3,79 +3,64 @@ import { singlePlayerStatRetrieval } from '../../../../api/nhlApi';
 
 export default function Goalies({ id }) {
   const [stats, setStats] = useState({});
+  const [playerInfo, setPlayerInfo] = useState({});
+  const [playerStats, setPlayerStats] = useState({});
   const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const setArrays = async () => {
-      setStats(await singlePlayerStatRetrieval(id));
+      const playerStats = await singlePlayerStatRetrieval(id);
+      setStats(playerStats);
+      const playerStatsHelper =
+        playerStats.playerStats.data.stats[0].splits[0].stat;
+      const playerInfoHelper = playerStats.playerInfo.data.people[0];
+      setPlayerInfo(playerInfoHelper);
+      setPlayerStats(playerStatsHelper);
       setLoading(true);
     };
     setArrays();
   }, []);
+  console.log(playerInfo);
+  console.log(playerStats);
   if (loading) {
     return (
       <div>
         <div>
-        <h2>
-          {stats.playerStats.data.stats[0].splits[0].season} Regular Season
-        </h2>
-          <img src={`http://nhl.bamcontent.com/images/headshots/current/168x168/${id}.jpg`} alt={`Human`} />
-          <h3>Name: {stats.playerInfo.data.people[0].fullName}</h3>
-          <h3>Height: {stats.playerInfo.data.people[0].height}</h3>
-          <h3>Weight: {stats.playerInfo.data.people[0].weight}lbs</h3>
+          <h2>
+            {stats.playerStats.data.stats[0].splits[0].season} Regular Season
+          </h2>
+          <img
+            src={`http://nhl.bamcontent.com/images/headshots/current/168x168/${id}.jpg`}
+            alt={`Human`}
+          />
+          <h3>Name: {playerInfo.fullName}</h3>
+          <h3>Height: {playerInfo.height}</h3>
+          <h3>Weight: {playerInfo.weight}lbs</h3>
+          <h3>Position: {playerInfo.primaryPosition.name}</h3>
+          <h3>Team: {playerInfo.currentTeam.name}</h3>
           <h3>
-            Position: {stats.playerInfo.data.people[0].primaryPosition.name}
-          </h3>
-          <h3>Team: {stats.playerInfo.data.people[0].currentTeam.name}</h3>
-          <h3>
-            DOB: {stats.playerInfo.data.people[0].birthDate} (
-            {stats.playerInfo.data.people[0].currentAge} years old)
+            DOB: {playerInfo.birthDate} ({playerInfo.currentAge} years old)
           </h3>
           <h3>
-            Birthplace: {stats.playerInfo.data.people[0].birthCity},{' '}
-            {stats.playerInfo.data.people[0].birthCountry}
+            Birthplace: {playerInfo.birthCity}, {playerInfo.birthCountry}
           </h3>
-          <h3>Hand: {stats.playerInfo.data.people[0].shootsCatches}</h3>
+          <h3>Hand: {playerInfo.shootsCatches}</h3>
         </div>
         <div>
+          <h3>Games Played: {playerStats.games}</h3>
+          <h3>Games Started: {playerStats.gamesStarted}</h3>
           <h3>
-            Games Played: {stats.playerStats.data.stats[0].splits[0].stat.games}
+            Goals Against Average: {playerStats.goalAgainstAverage.toFixed(2)}
           </h3>
-          <h3>
-            Games Started:{' '}
-            {stats.playerStats.data.stats[0].splits[0].stat.gamesStarted}
-          </h3>
-          <h3>
-            Goals Against Average:{' '}
-            {stats.playerStats.data.stats[0].splits[0].stat.goalsAgainstAverage}
-          </h3>
-          <h3>
-            Goals Against:{' '}
-            {stats.playerStats.data.stats[0].splits[0].stat.goalsAgainst}
-          </h3>
-          <h3>Wins: {stats.playerStats.data.stats[0].splits[0].stat.wins}</h3>
-          <h3>
-            Losses: {stats.playerStats.data.stats[0].splits[0].stat.losses}
-          </h3>
-          <h3>
-            Overtime Losses: {stats.playerStats.data.stats[0].splits[0].stat.ot}
-          </h3>
-          <h3>
-            Save Percentage:{' '}
-            {stats.playerStats.data.stats[0].splits[0].stat.savePercentage}
-          </h3>
-          <h3>Saves: {stats.playerStats.data.stats[0].splits[0].stat.saves}</h3>
-          <h3>
-            Shots Against:{' '}
-            {stats.playerStats.data.stats[0].splits[0].stat.shotsAgainst}
-          </h3>
-          <h3>
-            PowerPlay Saves:{' '}
-            {stats.playerStats.data.stats[0].splits[0].stat.powerPlaySaves}
-          </h3>
-          <h3>
-            PowerPlay Shots Against:{' '}
-            {stats.playerStats.data.stats[0].splits[0].stat.powerPlayShots}
-          </h3>
+          <h3>Goals Against: {playerStats.goalsAgainst}</h3>
+          <h3>Wins: {playerStats.wins}</h3>
+          <h3>Losses: {playerStats.losses}</h3>
+          <h3>Overtime Losses: {playerStats.ot}</h3>
+          <h3>Save Percentage: {playerStats.savePercentage}</h3>
+          <h3>Saves: {playerStats.saves}</h3>
+          <h3>Shots Against: {playerStats.shotsAgainst}</h3>
+          <h3>PowerPlay Saves: {playerStats.powerPlaySaves}</h3>
+          <h3>PowerPlay Shots Against: {playerStats.powerPlayShots}</h3>
         </div>
       </div>
     );

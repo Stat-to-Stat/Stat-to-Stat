a1fa52188f93f78a7587a1c57bdb4bec7d9165dc
Jeremy Cook <jcook61592@gmail.com>
2021-09-21 17:11:36 -0400

    pwa setup fully working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(
-    <Main />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Main />, document.getElementById('root'));
+
+serviceWorkerRegistration.register();

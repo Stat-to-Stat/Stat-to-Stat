503ff6ebea1207e116dba646e8d012fa1396e7c9
Jeremy Cook <jcook61592@gmail.com>
2021-09-23 01:16:34 -0400

    styling and home button added to all NHL pages

--- src/Pages/TeamToTeam/index.js
+++ src/Pages/TeamToTeam/index.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Search from './NHL/Search';
 import { nhlTeamRetrieval } from '../../api/nhlApi';
 import TeamStats from './NHL/TeamStats';
-
-import "./style.css"
+import { Link } from 'react-router-dom';
+import './style.css';
 
 export default function NHLTeamToTeam() {
   const [currentTeam, setCurrentTeam] = useState('');
@@ -36,38 +36,54 @@ export default function NHLTeamToTeam() {
   }, []);
   if (isLoaded) {
     return (
-      <div className="team-to-team-page-container ">
-        <div className="nhl-teams-container">
-        {currentTeamOne.id ? 
-        <div style={{backgroundColor:"#8feeffe9"}} className="nhl-team-stats">
-          <TeamStats id={currentTeamOne.id} />
-        </div>
-           : 
-          <div style={{backgroundColor:"#8feeffe9"}} className="nhl-team-search">
-          <Search
-          currentTeam={currentTeam}
-          setCurrentTeam={setCurrentTeamOne}
-          teamList={teamList}
-          />
-          </div>
-        }
-        {currentTeamTwo.id ? (
-          <div style={{backgroundColor:"#c8dbdfe9"}} className="nhl-team-stats">
-            <TeamStats id={currentTeamTwo.id} />
-          </div>
-        ) : (
-          <div style={{backgroundColor:"#c8dbdfe9"}} className="nhl-team-search">
-          <Search
-            currentTeam={currentTeam}
-            setCurrentTeam={setCurrentTeamTwo}
-            teamList={teamList}
-            />
-          </div>
-        )}
+      <div className='team-to-team-page-container '>
+        <div className='nhl-title'>Team to Team</div>
+        <Link to='/'>
+          <button className='home-button'>Home</button>
+        </Link>
+        <div className='nhl-teams-container'>
+          {currentTeamOne.id ? (
+            <div
+              style={{ backgroundColor: '#8feeffe9' }}
+              className='nhl-team-stats'
+            >
+              <TeamStats id={currentTeamOne.id} />
+            </div>
+          ) : (
+            <div
+              style={{ backgroundColor: '#8feeffe9' }}
+              className='nhl-team-search'
+            >
+              <Search
+                currentTeam={currentTeam}
+                setCurrentTeam={setCurrentTeamOne}
+                teamList={teamList}
+              />
+            </div>
+          )}
+          {currentTeamTwo.id ? (
+            <div
+              style={{ backgroundColor: '#c8dbdfe9' }}
+              className='nhl-team-stats'
+            >
+              <TeamStats id={currentTeamTwo.id} />
+            </div>
+          ) : (
+            <div
+              style={{ backgroundColor: '#c8dbdfe9' }}
+              className='nhl-team-search'
+            >
+              <Search
+                currentTeam={currentTeam}
+                setCurrentTeam={setCurrentTeamTwo}
+                teamList={teamList}
+              />
+            </div>
+          )}
         </div>
       </div>
     );
   } else {
-    return <div>LOADING BRO</div>;
+    return <div>Loading</div>;
   }
 }

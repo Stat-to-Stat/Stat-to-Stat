524532fac64dd6841e4f4f07fcd8a574c4d628f9
Jeremy Cook <jcook61592@gmail.com>
2021-09-23 13:29:57 -0400

    formatting buttons

--- src/Pages/SportsPage/NHLHomePage/index.js
+++ src/Pages/SportsPage/NHLHomePage/index.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import "./NHL.css"
-
-import Title from "./Components/Title"
-import Compare from "./Components/Compare"
+import React from 'react';
+import './NHL.css';
+import { Link } from 'react-router-dom';
+import Title from './Components/Title';
+import Compare from './Components/Compare';
 
 export default function NHLHomePage() {
-    return (
-        <div className="nhl-main-container">
-            <Title />
-            <Compare />
-        </div>
-    )
+  return (
+    <div className='nhl-main-container'>
+      <Link to='/'>
+        <button className='home-button'>Home</button>
+      </Link>
+      <Title />
+      <Compare />
+    </div>
+  );
 }

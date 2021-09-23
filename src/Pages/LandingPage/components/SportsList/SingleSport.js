b42f9b18a8ea04c3c8e2d24e5f5cafd6a943f567
Jeremy Cook <jcook61592@gmail.com>
2021-09-23 14:29:29 -0400

    landing page styling for logos

--- src/Pages/LandingPage/components/SportsList/SingleSport.js
+++ src/Pages/LandingPage/components/SportsList/SingleSport.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-function SingleSport({backgroundColor, link, img}) {
+function SingleSport({ backgroundColor, link, img, finished }) {
+  let history = useHistory();
 
-    let history = useHistory();
+  const mouseEnter = (e) => {
+    e.target.style.background = backgroundColor;
+  };
 
-    const mouseEnter = (e) => {
-        e.target.style.background = backgroundColor
-    }
+  const mouseLeave = (e) => {
+    e.target.style.background = '';
+  };
 
-    const mouseLeave = (e) => {
-        e.target.style.background = ''
-    }
+  const handleClick = () => {
+    history.push(link);
+  };
 
-    const handleClick = () => {
-        history.push(link)
-    }
-
-    return (
-        <div onClick={handleClick} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} className="landing-page-sport-logo">
-            <img src={`./SportLogos/${img}.png`} alt={img} />
-        </div>
-    )
+  return (
+    <div
+      onClick={handleClick}
+      onMouseEnter={mouseEnter}
+      onMouseLeave={mouseLeave}
+      className={`landing-page-sport-logo ${finished ? '' : 'coming-soon'}`}
+    >
+      <img src={`./SportLogos/${img}.png`} alt={img} />
+    </div>
+  );
 }
 
-export default SingleSport
+export default SingleSport;
--- src/Pages/LandingPage/components/SportsList/Sports.js
+++ src/Pages/LandingPage/components/SportsList/Sports.js
@@ -4,9 +4,10 @@ import SingleSport from './SingleSport';
 export default function Sports() {
   return (
     <div className='landing-page-sports-container'>
-      <SingleSport link='/NHL' img='NHL' />
+      <SingleSport link='/NHL' img='NHL' finished={true} />
       <SingleSport
         // link='/NBA'
+        finished={false}
         img='NBA'
       />
       <SingleSport img='NFL' />

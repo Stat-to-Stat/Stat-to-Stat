f50972f1fd721d86615a7dfcf01bcb6d55992ad0
Shawn Gay <Shawn.Gay.Jr@gmail.com>
2021-09-24 13:48:11 -0500

    Styled landing page background to change on sport hover

--- src/Pages/LandingPage/components/SportsList/SingleSport.js
+++ src/Pages/LandingPage/components/SportsList/SingleSport.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-function SingleSport({ backgroundColor, link, img, finished }) {
+function SingleSport({background, backgroundColor, link, img, finished }) {
   let history = useHistory();
+
+  const backgroundStyle = "radial-gradient(163.19% 93.36% at 50% 46.95%,#28282c 0%,#1a1a1d 34.38%,#2c2c30 61.98%,#1a1a1d 100%)"
 
   const mouseEnter = (e) => {
     e.target.style.background = backgroundColor;
+    const mainContaier = document.getElementsByClassName('landing-page-container')[0]
+    mainContaier.style.backgroundImage = `url(./SportLogos/${background}.jpeg)`
+    mainContaier.style.backgroundSize = "cover"
+    mainContaier.style.backgroundRepeat = "no-repeat"
+    mainContaier.style.backgroundPosition = "center";
   };
 
   const mouseLeave = (e) => {
     e.target.style.background = '';
+    document.getElementsByClassName('landing-page-container')[0]
+    .style.background = backgroundStyle
   };
 
   const handleClick = () => {

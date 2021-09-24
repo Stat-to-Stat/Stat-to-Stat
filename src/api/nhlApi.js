a01ecfe93f3fef8b6d3d5134e471b6340fb2c74d
Jeremy Cook <jcook61592@gmail.com>
2021-09-24 16:57:04 -0400

    final checks

--- src/api/nhlApi.js
+++ src/api/nhlApi.js
@@ -79,7 +79,7 @@ export const singleNhlPlayerRetrieval = (id) => {
 };
 
 // Single Player Stats
-// UPDATE TO 2021-2022 ONCE REGULAR SEASON STARTS!!!!!!!
+// UPDATE TO 2021-2022 ONCE REGULAR SEASON STARTS!!!!!!!!
 export const singlePlayerStatRetrieval = async (id, season = 20202021) => {
   let playerStats = null;
   let playerInfo = null;

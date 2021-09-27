36af3ddf0cfd0941b2aefd87a0a309423ae4a637
Jeremy Cook <jcook61592@gmail.com>
2021-09-27 18:43:12 -0400

    removal of toml and service worker tests

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -21,21 +21,14 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  window.addEventListener('activate', function (event) {
-    event.waitUntil(
-      caches.keys().then(function (cachesNames) {
-        return Promise.all(
-          cachesNames
-            .filter(function (cacheName) {
-              return true;
-            })
-            .map(function (cacheName) {
-              return caches.delete(cacheName);
-            })
-        );
-      })
-    );
-  });
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    // The URL constructor is available in all browsers that support SW.
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      // Our service worker won't work if PUBLIC_URL is on a different origin
+      // from what our page is served on. This might happen if a CDN is used to
+      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      return;
     }
 
     window.addEventListener('load', () => {

8d203196bb0a59942e3b1f42ca9f7b2bb8f116b0
Jeremy Cook <jcook61592@gmail.com>
2021-09-27 19:05:52 -0400

    removed service workers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
-
-serviceWorkerRegistration.register();

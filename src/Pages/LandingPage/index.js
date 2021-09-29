a7b176848189ba85c2c5eba43905561de92742c6
Jeremy Cook <jcook61592@gmail.com>
2021-09-29 17:37:46 -0400

    removing clutter and unused code

--- src/Pages/LandingPage/index.js
+++ src/Pages/LandingPage/index.js
@@ -1,24 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './landing.css';
-import axios from 'axios';
 import { Title, Sports } from './components';
 
 export default function LandingPage() {
-  useEffect(() => {
-    let playerArr = [];
-    axios
-      .get('https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster')
-      .then((res) => {
-        const teams = res.data.teams;
-        for (const team of teams) {
-          let players = team.roster.roster;
-          for (const player of players) {
-            playerArr.push(player.person.fullName);
-          }
-        }
-      });
-  }, []);
-
   return (
     <div className='landing-page-container'>
       <Title />

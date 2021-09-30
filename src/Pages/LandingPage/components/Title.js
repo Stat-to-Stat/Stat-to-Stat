cf42b5fce448e7576ecc5ac1b8f303153012d3bc
Jeremy Cook <jcook61592@gmail.com>
2021-09-30 15:57:27 -0400

    nba api calls

--- src/Pages/LandingPage/components/Title.js
+++ src/Pages/LandingPage/components/Title.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import {
+  nbaPlayerStatsRetrieval,
+  nbaPlayerIdRetrieval,
+  nbaTeamsRetrieval,
+} from '../../../api/nbaApi';
 
 export default function Title() {
+  useEffect(() => {
+    const setStats = async () => {
+      const playerStats = await nbaPlayerStatsRetrieval(246);
+      const playerInfo = await nbaPlayerIdRetrieval('Jokic');
+      const teamList = await nbaTeamsRetrieval();
+      // console.log(playerStats);
+      // console.log(playerInfo);
+      console.log(teamList);
+    };
+    setStats();
+  });
+
   return (
-    <div className="landing-page-title-container">
+    <div className='landing-page-title-container'>
       <div className='Landing-Page-Title--about'>
         <Link to='/aboutus'>
           <button type='submit' className='aboutButton'>
